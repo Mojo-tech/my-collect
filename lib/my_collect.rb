e99febd393814074588f177bff97a63106bcc8e3
Monique <momo_89@ymail.com>
2021-01-24 16:28:29 -0600

    commit tree 6af0ba0f817d3c5ebc6c40b234b6974423198e44

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
 
+def my_collect(array)
+    i = 0
+    name_collection = []
+    while i < array.length
+        name_collection << yield(array[i])
+        i += 1
+    end
+     name_collection
+end
 
+my_collect(students) {|i| i.split(" ").first}
